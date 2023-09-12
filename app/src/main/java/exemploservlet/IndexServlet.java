63967e87671bc82199f96a4674321316c766ff2f
diogo-gb Iwamoto <diogoiwamoto7999@gmail.com>
2023-09-12 18:45:51 +0000

    utilizando JSP como resposta

---
+++ app/src/main/java/exemploservlet/IndexServlet.java
@@ -0,0 +1,23 @@
+package exemploservlet;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "indexServlet", urlPatterns ="/")
+public class IndexServlet extends HttpServlet {
+    public void doGet(
+        HttpServletRequest request,
+        HttpServletResponse response)
+        throws ServletException, IOException {
+            String n = request.getParameter("nome");
+            request.setAttribute("nome", n);
+           // response.getWriter().print("Olá" + n);
+           request.getRequestDispatcher(
+            "/index.jsp").forward(request, response);
+    }
+}
